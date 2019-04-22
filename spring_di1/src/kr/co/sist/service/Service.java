02b388305ae14de4232a7393ff072fe6584d5691
hyewon0218 <45455420+hyewon0218@users.noreply.github.com>
2019-04-22 18:53:21 +0900

    day0422

---
+++ spring_di1/src/kr/co/sist/service/Service.java
@@ -0,0 +1,10 @@
+package kr.co.sist.service;
+
+import java.util.List;
+
+import kr.co.sist.vo.EmpVo;
+
+public interface Service {
+	public void addEmp(EmpVo ev);
+	public List<EmpVo> searchEmp();
+}
